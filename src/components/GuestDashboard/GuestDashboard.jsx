77cb10743c00df12abf961698b373f8c65550ef0
jackiecosa <jackie@benincosa.com>
2025-10-21 13:23:32 -0700

    dashboard and guest dash separated

--- src/components/GuestDashboard/GuestDashboard.jsx
+++ src/components/GuestDashboard/GuestDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Dashboard = () => {
     return(
-        <h1 className="title">Dashboard</h1>
+        <h1 className="title">Guest Dashboard</h1>
     )
 }
 export default Dashboard;
