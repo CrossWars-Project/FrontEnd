7f957f68e101a499d012a34b393e6e869d68d81e
akpettis2 <annakatherinepettis@gmail.com>
2025-10-23 13:34:12 -0700

    Added guest dashboard with functionality to go between placeholder play screens

---
+++ src/components/Stats/Stats.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const Dashboard = () => {
+    return(
+        <h1 className="title">Stats Dashboard</h1>
+    )
+}
+export default Dashboard;
--- src/router.jsx
+++ src/router.jsx
@@ -3,10 +3,17 @@ import LoginSignup from "./components/LoginSignup/LoginSignup"
 import App from "./App"
 import Dashboard from "./components/Dashboard/Dashboard"
 import GuestDashboard from "./components/GuestDashboard/GuestDashboard"
+import BattleScreen from "./components/BattleScreen/BattleScreen"
+import SoloPlay from "./components/SoloPlay/SoloPlay"
+import Stats from "./components/Stats/Stats"
 
 export const router = createBrowserRouter([
     {path: "/", element: <App /> },
     {path: "/loginSignup", element: <LoginSignup /> },
     {path: "/dashboard", element: <Dashboard />},
     {path: "/guestDashboard", element: <GuestDashboard />},
+    {path: "/battle", element: <BattleScreen />},
+    {path: "/solo", element: <SoloPlay />},
+    {path: "/stats", element: <Stats />},
+
 ])
