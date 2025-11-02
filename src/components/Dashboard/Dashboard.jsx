98697215a602975dd57cbbb8c5f9f4464ed037a3
akpettis2 <annakatherinepettis@gmail.com>
2025-11-02 11:50:57 -0800

    saving current work

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,3 @@
-/* import React, { useState } from "react";
-
-const Dashboard = () => {
-    return(
-        <h1 className="title">Guest Dashboard</h1>
-    )
-}
-export default Dashboard; */
 import React from 'react';
 import './Dashboard.css';
 import { useNavigate } from 'react-router-dom';
