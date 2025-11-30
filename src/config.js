ac241e898eb56a40a2c927e077fdccb4a504acd3
Danielle <dmaddo@uw.edu>
2025-11-29 16:09:15 -0800

    clarifying comments

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 // API configuration
 // eslint-disable-next-line import/prefer-default-export
 export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+//Only uses deployed backend if VITE_API_URL is explicitly set in .env file
